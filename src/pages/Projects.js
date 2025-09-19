521178eb9b55e763b1487336368304a8b4f408c9
gayangi <gayangidevindi@gmail.com>
2025-09-19 16:39:54 +0530

    Add pet projects and images

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,45 +1,62 @@
 import React from 'react';
 import './Projects.css';
 
-
 import compostImg from '../assets/compost.jpg';
 import coffeeImg from '../assets/coffee.png';
 import flutterImg from '../assets/flutter.jpg';
 import figmaImg from '../assets/figma.jpg';
 import mitImg from '../assets/mit.jpeg';
 import zoneImg from '../assets/zone.jpeg';
+import petproject1 from '../assets/petproject1.png';
+import petproject2 from '../assets/petproject2.png';
 
 export default function Projects() {
   const projects = [
     {
       title: 'Rapid Compost Maker',
-      description: 'First-year hardware group project: Making compost within 24 hours. Use ESP32 DHT22,Ds18B20,Load cell,MQ Ch4 sensor',
-      image: compostImg
+      description:
+        'First-year hardware group project: Making compost within 24 hours. Used ESP32, DHT22, DS18B20, Load cell, MQ CH4 sensor.',
+      image: compostImg,
     },
     {
       title: 'Coffee Shop Website',
-      description: 'Website project for a coffee shop for second semester module. Ude HTML,PHP,CSS,JS',
-      image: coffeeImg
+      description:
+        'Website project for a coffee shop (second semester module). Built using HTML, PHP, CSS, and JavaScript.',
+      image: coffeeImg,
     },
     {
       title: 'Mobile App with Flutter',
-      description: 'Creating mobile applications using Flutter framework.',
-      image: flutterImg
+      description: 'Creating mobile applications using the Flutter framework.',
+      image: flutterImg,
     },
     {
       title: 'App Design with Figma',
       description: 'Designing app interfaces and UX using Figma.',
-      image: figmaImg
+      image: figmaImg,
     },
     {
       title: 'App with MIT App Inventor',
-      description: 'Building functional apps using MIT App Inventor platform.',
-      image: mitImg
+      description:
+        'Building functional apps using the MIT App Inventor platform.',
+      image: mitImg,
     },
     {
       title: 'Team Visit to Zone 24x7',
-      description: 'Guiding my team as a management module project to visit Zone 24x7.We have to study about the company management and submit a repote and also do a presentation ',
-      image: zoneImg
+      description:
+        'Guided my team in a management module project to visit Zone 24x7. We studied the company management, submitted a report, and presented findings.',
+      image: zoneImg,
+    },
+    {
+      title: 'Pet Shop Website',
+      description:
+        'Developed a responsive website for a pet shop. Features product listings, categories, and basic shopping flow.',
+      image: petproject1,
+    },
+    {
+      title: 'Digital Devices Website',
+      description:
+        'Created a modern website to showcase digital devices. Focused on clean UI, product cards, and interactive elements.',
+      image: petproject2,
     },
   ];
 
@@ -60,5 +77,5 @@ export default function Projects() {
         ))}
       </div>
     </div>
-  )
+  );
 }
